49f8be2ae40bbda64028e29e113b7c7a566d0338
hayden <2867571834@qq.com>
2025-09-06 16:32:18 +0800

    finsh the most of LruCache(normal), finish some testCase

--- LruCache.h
+++ LruCache.h
@@ -2,7 +2,7 @@
  * @Author: hayden 2867571834@qq.com
  * @Date: 2025-08-31 13:50:46
  * @LastEditors: hayden 2867571834@qq.com
- * @LastEditTime: 2025-09-04 15:10:29
+ * @LastEditTime: 2025-09-06 16:02:07
  * @FilePath: \CacheSystem\LruCache.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,21 +12,23 @@
 #include <unordered_map>  // 使用哈希表
 #include <list>   // 使用双向链表
 
-namespace CacheSystemPolicy
+#include "CachePolicy.h"
+
+namespace CacheSystem
 {
 
     // 前向说明
     template <typename Key, typename Value>
     calss LruCache;
 
+    // 缓存节点类，可以看做是双向缓存链表的节点
     template <typename Key, typename Value>
     class LruNode
     {
     public:
         LruNode(Key key, Value value)
             : key_(key), value_(value), accessCount_(1)
-        {
-        }
+        {}
 
         // 访问器
         Key getKey() const { return key_; }
@@ -46,8 +48,10 @@ namespace CacheSystemPolicy
         friend class LruCache<Key, Value>;
     };
 
+
+
     template <typename Key, typename Value>
-    class LruCache
+    class LruCache: public CachePolicy<Key, Value>
     {
     public:
         // 别名声明
@@ -55,6 +59,7 @@ namespace CacheSystemPolicy
         using NodePtr = std::shared_ptr<LruNodeType>;
         using NodeMap = std::unorered_map<Key, NodePtr>;
 
+        // 构造函数， 传入缓存容量
         LruCache(int capacity)
             : capacity_(capacity)
         {
@@ -63,7 +68,7 @@ namespace CacheSystemPolicy
 
         ~LruCache() override = default;
 
-        // 添加缓存
+        // 添加缓存(重写基类)
         void put(Key key, Value value) override
         {
             if (capacity_ <= 0) // 缓存已满无法添加
@@ -72,7 +77,7 @@ namespace CacheSystemPolicy
             std::lock_guard<std::mutex> lock(mutex_);
             auto it = nodeMap_.find(key);
             if (it != nodeMap_.end()) {
-                // 如果在当前容器中,则更新value,并调用get方法，代表该数据刚被访问
+                // key在缓存中时，更新它对应的值，并把它标记为刚刚被访问过
                 updateExistingNode(it -> second, value);
                 return ;
             }
@@ -80,10 +85,90 @@ namespace CacheSystemPolicy
             addNewNode(key, value);
         }
 
+        bool get(Key key, Value& value) override
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            auto it = nodeMap_.find(key);
+            if (it != nodeMap_.end()) {
+                moveToMostRecent(it -> second);
+                value = it -> second -> getValue();
+                reurn true;
+            }
+            return false;
+        }
+
+        Value get(Key key) override
+        {
+            Value value{};   // 1. 创建一个默认构造的 Value 对象（比如 int 就是 0）
+            get(key, value);  // 2. 调用 bool get(Key, Value&) 版本，尝试读取缓存
+            return value;   // 3. 返回 value（不管有没有找到）这里也许可以优化一下
+        } 
+
+        // 删除指定元素
+        void remove(Key key)
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            auto it = nodeMap_.find(key);
+            if (it != nodeMap_.end()) {
+                removeNode(it -> second);
+                nodeMap_.erase(it);
+            }
+        }
+
+
     private:
-        void initialize() {
-            // 造一个假头节点（方便处理边界），里面不存真实数据
+        void initialize() 
+        {
+            // 造一个首尾虚拟节点（方便处理边界），里面不存真实数据
             dummyHead_ = std::make_shared<LruNodeType>(Key(), Value());
+            dummyTail_ = std::make_shared<LruNodeType>(Key(), Value());
+            dummyHead_ -> next_ = dummyTail_;
+            dummyTail_ -> prev_ = dummyHead_;
+        }
+
+        void updateExistingNode(NodePtr node, const Value& value) {
+            // node : 已存在于链表中的那个节点, 传入它是为了修改链表来记录最近使用
+            // value : 要写入的新值
+            node -> setValue(value);
+            moveToMostRecent(node);
+        }
+        
+        void addNewNode(const Key& key, const Value& value) {
+            //  没有缓存空间了，驱逐一个
+            if (nodeMap_.size() >= capacity_) {
+                evictLeastRecent();
+            }
+            NodePtr newNode = std::make_shared<LruNodeType>(key, value);
+            insertNode(newNode);
+            nodeMap_[key] = newNode;  
+        }
+
+
+        // 将该节点移动到最新的位置
+        void moveToMostRecent(NodePtr node) {
+            removeNode(node);
+            insertNode(node);
+        }
+
+        void removeNode(NodePtr node) 
+        {
+            //待补充
+            cout << "待补充" << endl;
+        }
+
+        // 从尾部插入指针
+        void insertNode(NodePtr node) {
+            node -> next_ = dumyTail_;
+            node -> prev_ = dummyTail_ -> prev_;
+            dummyTail_ -> prev_.lock() -> next_ = node; // 使用lock()获取shared_ptr
+            dummyTail_->prev_ = node;
+        }
+
+        // 驱逐最近最少访问
+        void evictLeastRecent() {
+            NodePtr leastRecent  = dummyHead_ -> next_;
+            removeNode(leastRecent);
+            nodeMap_.erase(leastRecent -> getKey());
         }
 
     private:
--- testCase.cpp
+++ testCase.cpp
@@ -2,7 +2,7 @@
  * @Author: hayden 2867571834@qq.com
  * @Date: 2025-08-30 20:37:27
  * @LastEditors: hayden 2867571834@qq.com
- * @LastEditTime: 2025-09-01 15:48:31
+ * @LastEditTime: 2025-09-06 16:11:40
  * @FilePath: \CacheSystem\testCase.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,7 @@
 #include <chrono>  // 包含Timer
 #include <string>
 #include <array>
+#include <vector>
 
 // 计时器工具类，测量某段代码执行时间
 class Timer {
@@ -26,6 +27,7 @@ private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start_;
 };
 
+
 void testHotDataAccess() {
     std::cout << "\n === 测试场景1: 热点数据访问测试 ===" << std::endl;
 
@@ -34,14 +36,37 @@ void testHotDataAccess() {
     const int HOT_KEYS = 20;         // 热点数据数量
     const int COLD_KEYS = 5000;      // 冷数据数量
 
-    CacheSystemPolicy::LruCache<int, std::string> lru(CAPACITY);
+    CacheSystem::LruCache<int, std::string> lru(CAPACITY);
 
     // 随机决定 读还是写操作 访问热点 还是 冷数据
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    // 把不同的缓存实现“打包”成统一接口
-    std::array<CacheSystemPolicy::>
+    // 基类指针指向派生类对象”，从而把 不同淘汰策略的缓存实例 统一塞进同一个容器里，方便后续 批量测试、统一调用
+    std::array<CacheSystem::CachePolicy<int, std::string>*, 1> caches = {&lru};
+    std::vector<int> hits(1, 0);  //命中计数器
+    std::vector<int> get_operations(1, 0);  //总查询计数器
+    std::vector<std::string> names = {"LRU"}; // 算法名称
+
+    // 为所有的缓存对象进行相同的操作序列测试
+    for (int i = 0; i < caches.size(); ++i) {
+        // 先预热缓存，插入一些数据
+        for (int key = 0; key < HOT_KEYS; ++key) {
+            std::string value = "value" + std::to_string(key);
+            caches[i] -> put(key, value);
+        }
+
+        // 交替进行put和get操作, 模拟真实场景
+        for (int op = 0; op < OPERATIONS; ++op) {
+            // 大多数缓存系统中读操作比写操作频繁
+            // 所以设置30%概率进行写操作
+            bool isPut = (gen() % 100 < 30);
+            int key;
+
+            // 70%概率访问热点数据，30%概率访问冷数据
+            
+        }
+    }
 }
 
 int main() {
